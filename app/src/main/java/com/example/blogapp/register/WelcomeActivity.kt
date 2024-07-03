dd38ae24d59fc657ec491a604e4fab7fc969cced
Sagar Birhade <spatil7249@gmail.com>
2024-07-03 22:23:46 +0530

    commit done

--- app/src/main/java/com/example/blogapp/register/WelcomeActivity.kt
+++ app/src/main/java/com/example/blogapp/register/WelcomeActivity.kt
@@ -1,22 +1,21 @@
-package com.example.blogapp
+package com.example.blogapp.register
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blogapp.R
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
     }
 }
